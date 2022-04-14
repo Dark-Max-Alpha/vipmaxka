5dceb72dbc8e1ed3845545bebef92195943a1503
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-04-14 20:19:44 +0530

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,22 @@
+/* Copyright (C) 2021 HIRUWA
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+T-REX HIRUWA
+*/
+
+function successfullMessage(msg) {
+    return "⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
