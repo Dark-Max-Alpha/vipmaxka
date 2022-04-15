3081d2d2843156da4e5df474a16f394ee4e6324f
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-04-15 08:00:15 +0530

    Update Base.js

--- VIP/Base.js
+++ VIP/Base.js
@@ -1,10 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
+/* Copyright (C) 2021 ⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️ HIRUWA
 */
+
 
 'use strict';
 
